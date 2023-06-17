5cb007466a9c5646acac9a07c9022dac421187d1
Hebilicious <xsh4k3@gmail.com>
2023-06-17 21:44:29 +0700

    chore: console statement

--- playgrounds/vorms-zod/server/actions/login.ts
+++ playgrounds/vorms-zod/server/actions/login.ts
@@ -3,6 +3,7 @@ import { actionResponse, defineFormActions, getFormData } from "#form-actions"
 export default defineFormActions({
   default: async (event) => {
     const formData = await getFormData(event)
+    // eslint-disable-next-line no-console
     console.log("Login Received ...", formData.entries())
     return actionResponse(event, { formData })
   }
