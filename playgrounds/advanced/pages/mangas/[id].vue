69361b6b9d0764e2ea76482c40bac3cae7bb7f45
Hebilicious <xsh4k3@gmail.com>
2023-08-09 04:30:56 +0700

    chore: add server block and param loader example

---
+++ playgrounds/advanced/pages/mangas/[id].vue
@@ -0,0 +1,16 @@
+<server lang="ts" path="mangas">
+export const loader = defineServerLoader(async (event) => {
+  return { mangas: ["one-piece"], params: getQuery(event) }
+})
+</server>
+
+<script setup lang="ts">
+const { result } = await useLoader("mangas")
+</script>
+
+<template>
+  <div>
+    <h1>Mangas</h1>
+    {{ result }}
+  </div>
+</template>
