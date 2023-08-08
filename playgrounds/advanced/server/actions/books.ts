68179577055acfbb628fab0cf6e2db2b341a088e
Hebilicious <xsh4k3@gmail.com>
2023-08-09 04:19:21 +0700

    docs: add params example

--- playgrounds/advanced/server/actions/books.ts
+++ playgrounds/advanced/server/actions/books.ts
@@ -1,3 +1,4 @@
-export const loader = defineServerLoader(async () => {
-  return { books: ["title"], manybooks: [1, 2, 3, 4, 5] }
+export const loader = defineServerLoader(async (event) => {
+  const params = getQuery(event)
+  return { books: ["title"], manybooks: [1, 2, 3, 4, 5], params }
 })
