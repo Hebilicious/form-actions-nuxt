5b861ca6c368adb122a1ee0cf1de680d360c802b
Hebilicious <xsh4k3@gmail.com>
2023-08-30 03:43:25 +0700

    docs: copy nuxt image docs

---
+++ docs/layouts/docs.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const { mapContentNavigation } = useElementsHelpers()
+const navigation = await useNavigation()
+</script>
+
+<template>
+  <div>
+    <UMain>
+      <UContainer>
+        <UPage>
+          <template #left>
+            <UAside>
+              <template #top>
+                <UDocsSearchButton size="md" />
+              </template>
+              <UNavigationTree :links="mapContentNavigation(navigation)" />
+            </UAside>
+          </template>
+          <slot />
+        </UPage>
+      </UContainer>
+    </UMain>
+  </div>
+</template>
