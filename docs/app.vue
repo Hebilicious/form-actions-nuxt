142f4b3d7b382c82b2f910d7991afe1018ecae9d
Hebilicious <xsh4k3@gmail.com>
2023-11-10 06:06:23 +0700

    fix: remove old css

--- docs/app.vue
+++ docs/app.vue
@@ -80,21 +80,3 @@ const navigation = await useNavigation()
     <LazyUDocsSearch :files="files" :navigation="navigation" :links="links" />
   </ClientOnly>
 </template>
-
-<style>
-html.dark {
-  color-scheme: dark;
-}
-
-.shiki {
-  padding: 0.6rem;
-  border-radius: 0.2rem;
-  border: 1px solid #8882;
-}
-
-html.dark .shiki,
-html.dark .shiki span {
-  color: var(--s-dark) !important;
-  background-color: var(--s-dark-bg) !important;
-}
-</style>
